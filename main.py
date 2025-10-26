00cb0983ce971c071dd07c9dd88ff42281325e35
Chris Doggett <doggettc@gmail.com>
2025-10-26 00:23:40 -0500

    Figured out newlines/double-quotes in CSV and added non-dry-run arg

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import os
 
@@ -6,29 +7,81 @@ from imageio.v2 import imread
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        prog="MovieSkeeter",
+        description="Skeets a movie review on BlueSky",
+    )
+    parser.add_argument(
+        "-x",
+        dest="execute",
+        action="store_true",
+        help="Actually run, otherwise just print what would be posted.",
+    )
+    parsed_args = parser.parse_args()
+    print(parsed_args)
+
+    client = build_bsky_client()
+
+    for row in read_csv_rows("data/movies.csv"):
+        body_text = build_body_text(row)
+        alt_text = build_alt_text(row)
+        image, aspect_ratio = build_image_and_aspect_ratio(row)
+        if parsed_args.execute:
+            send_skeet(client, body_text, img_data, alt_text, aspect_ratio)
+
+
+def build_bsky_client():
     client = Client()
     client.login(os.environ.get("BSKY_USER"), os.environ.get("BSKY_PASS"))
-    with open("data/movies.csv", mode="r") as csv_file:
+    return client
+
+
+def read_csv_rows(filename):
+    with open(filename, mode="r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            text = f"{row["year"]} watch #{row["number"]}: {row["title"]} ({row["release_year"]})\n\n{row["review"]}"
-            alt_text = row["alt_text"]
-            print(text)
-            print(f"\n{alt_text}")
-            with open(row["image_path"], "rb") as f:
-                img_data = f.read()
+            yield row
 
-            img = imread(row["image_path"])
-            print(f"Aspect ratio: {img.shape}")
-            post = client.send_image(
-                text=text,
-                image=img_data,
-                image_alt=alt_text,
-                image_aspect_ratio=models.AppBskyEmbedDefs.AspectRatio(
-                    height=img.shape[0], width=img.shape[1]
-                ),
-            )
-            print(f"\n{post}")
+
+def send_skeet(client, body_text, img_data, alt_text, aspect_ratio):
+    post = client.send_image(
+        text=body_text,
+        image=img_data,
+        image_alt=alt_text,
+        image_aspect_ratio=aspect_ratio,
+    )
+    print(f"\nPost info: {post}")
+    print("---------------------")
+
+
+def build_body_text(row):
+    year = row["year"]
+    num = row["number"]
+    title = row["title"]
+    release_year = row["release_year"]
+    review = row["review"].replace("\\n", "\n")
+    body_text = f"{year} watch #{num}: {title} ({release_year})\n\n{review}"
+    print(body_text)
+    return body_text
+
+
+def build_alt_text(row):
+    alt_text = row["alt_text"]
+    print(f"\nALT text: {alt_text}")
+    return alt_text
+
+
+def build_image_and_aspect_ratio(row):
+    with open(row["image_path"], "rb") as f:
+        img_data = f.read()
+    img = imread(row["image_path"])
+
+    print(f"Image size: {len(img_data)}")
+    print(f"Aspect ratio: {img.shape[1]}x{img.shape[0]}")
+
+    return img_data, models.AppBskyEmbedDefs.AspectRatio(
+        height=img.shape[0], width=img.shape[1]
+    )
 
 
 if __name__ == "__main__":
